4aabf4d56aed31a25f06ee408796530199a39579
Kobe LaPrade <41244097+kobster68@users.noreply.github.com>
2026-09-12 17:41:03 -0400

    ref: stubbify metrics bat file

--- courseProjectCode/Metrics/run-metrics.bat
+++ courseProjectCode/Metrics/run-metrics.bat
@@ -1,16 +1,1 @@
-@echo off
-
-cd /d "%~dp0"
-
-if not exist "build" mkdir "build"
-
-javac -d "build" "src\metrics\Main.java" "src\metrics\CodeStructureMetrics.java" "src\metrics\TestabilityMetrics.java"
-
-if errorlevel 1 (
-    echo Compilation failed.
-    exit /b 1
-)
-
-java -cp "build" metrics.Main
-
-pause
+# TODO: implement or delete this script

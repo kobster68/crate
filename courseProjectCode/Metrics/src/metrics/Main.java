67c1bcedc61bfa8bc422774597b1780c55345e1f
Kobe LaPrade <41244097+kobster68@users.noreply.github.com>
2026-09-12 00:46:29 -0400

    feat: created basic main function with selectors

---
+++ courseProjectCode/Metrics/src/metrics/Main.java
@@ -0,0 +1,45 @@
+package metrics;
+
+import java.util.Scanner;
+
+import metrics.CodeStructureMetrics;
+import metrics.TestabilityMetrics;
+
+public class Main {
+	
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+
+		System.out.println("1 - Code Structure Metrics | 2 - Testability Metrics");
+		System.out.println("3 - Both | 4 - Exit");
+
+		System.out.print("Enter your choice: ");
+
+		String input = scanner.nextLine();
+		int choice = Integer.parseInt(input);
+
+		scanner.close();
+
+		switch (choice) {
+			case 1:
+				CodeStructureMetrics.linesOfCode();
+				CodeStructureMetrics.commentDensity();
+				break;
+			case 2:
+				TestabilityMetrics.testCount();
+				TestabilityMetrics.testCoverage();
+				break;
+			case 3:
+				CodeStructureMetrics.linesOfCode();
+				CodeStructureMetrics.commentDensity();
+				TestabilityMetrics.testCount();
+				TestabilityMetrics.testCoverage();
+				break;
+			case 4:
+				System.out.println("Exiting the program.");
+				break;
+
+		}
+	}
+	
+}

068080b8dd7748bf4c407e7a934ba883a2f2554b
Kobe LaPrade <41244097+kobster68@users.noreply.github.com>
2026-09-24 20:25:34 -0400

    test: null timezone rejection test

--- server/src/test/java/io/crate/expression/scalar/TimeZoneParserTest.java
+++ server/src/test/java/io/crate/expression/scalar/TimeZoneParserTest.java
@@ -1,5 +1,15 @@
 package io.crate.expression.scalar;
 
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import org.junit.Test;
+
 public class TimeZoneParserTest {
 
+    @Test
+    public void test_null_timezone_is_rejected() {
+        assertThatThrownBy(() -> TimeZoneParser.parseTimeZone(null))
+            .isInstanceOf(IllegalArgumentException.class)
+            .hasMessage("invalid time zone value NULL");
+    }
 }
